e67ed61e0cf35eef43e4c39bfa33eebf38458bed
Aswini Chandramohan <aswini.chandramohan@seattlecentral.edu>
2019-02-24 01:02:03 -0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,6 @@
             
     <!-- Footer -->
     <footer>
-      <p class="copyrights"><small>&copy; 2018 - <?php echo date("Y")?> by ITC250 W19 Group 1, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
       <p class="copyrights"><small>&copy; 2018 - <?php echo date("Y")?> by ITC250 W19 Group 2, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
       <img src="image/image1.jpg" alt="Thermometer">
     </footer>
